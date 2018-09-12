62eb85bace0e083eb37b711602c010e1dbb24b5e
ndxbn <sitri.ndxbn@gmail.com>
2018-09-13 02:47:16 +0900

    fix invalid "Attribute Starts With Selector"

--- scroll.js
+++ scroll.js
@@ -1,6 +1,6 @@
 $(function() {
   // #で始まるアンカーをクリックした場合に処理
-  $("a[href^=#]").click(function() {
+  $("a[href^='#']").click(function() {
     // スクロールの速度
     var speed = 1200; // ミリ秒
     // アンカーの値取得
