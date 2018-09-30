c0c62a91afe907b801d5434995257c8c6e080a4a
ndxbn <sitri.ndxbn@gmail.com>
2018-09-30 22:26:15 +0900

    不要なコメントを削ったり、変数名を直したり。

--- button.js
+++ button.js
@@ -1,24 +1,22 @@
-// グローバル変数
 var syncerSounds = {
   flag: {},
   currentTime: null
 };
 
-// 即時関数
-document.addEventListener("DOMContentLoaded", function() {
-  // 設定
-  var setClass = "sounds"; // ボタン要素のクラス名
-  var setDir = "/sounds/"; // 音声ファイルがあるフォルダ(最後は[/])
-  var setStopButtonId = "stop-button"; // 停止ボタンに付けるID
+document.addEventListener("DOMContentLoaded", function () {
 
-  // クラス名が付いた要素を取得する
-  var sounds = document.getElementsByClassName(setClass);
+  /** @var ボタン要素のクラス名 */
+  var soundButtonClassName = "sounds";
+  /** @var 音声ファイルがあるフォルダ。末尾に `/` を含む */
+  var soundsBasePath = "/sounds/";
+  /** @var 停止ボタンに付ける ID */
+  var stopButtonId = "stop-button";
 
-  // 全ての要素にクリックイベントを設定する
+  var sounds = document.getElementsByClassName(soundButtonClassName);
+
   for (var i = 0, l = sounds.length; l > i; i++) {
-    // クリックイベントの設定
-    sounds[i].onclick = function() {
-      // ファイル名の取得
+    sounds[i].onclick = function () {
+
       var file = this.getAttribute("data-file");
 
       // 一度生成したエレメントは生成しない
@@ -26,40 +24,35 @@ document.addEventListener("DOMContentLoaded", function() {
         typeof syncerSounds.flag[file] == "undefined" ||
         syncerSounds.flag[file] != 1
       ) {
-        // 生成するエレメントの調整
         var audio = document.createElement("audio");
 
-        // エレメントのIDを指定
         audio.id = file;
 
-        // ブラウザが[.mp3]に対応している場合は[.mp3]を読み込む
         if (audio.canPlayType("audio/mp3")) {
-          audio.src = setDir + file + ".mp3";
+          audio.src = soundsBasePath + file + ".mp3";
         }
 
-        // [audio]を生成する
         document.body.appendChild(audio);
       }
 
       // 初回再生以外だったら音声ファイルを巻き戻す
       stopCurrentSound();
 
-      // 音声ファイルを再生[play()]する
       document.getElementById(file).play();
 
-      // 最近再生した音声としてセット
       syncerSounds.currentTime = file;
 
       // 初回再生が終わった判定用に[syncerSounds.flag]の値を0から1に変更する
       // エレメントを何度も生成しないようにするため
       syncerSounds.flag[file] = 1;
 
-      // 終了
       return false;
     };
   }
 
-  // 前回の音声を停止する関数
+  /**
+   * 前回の音声を停止する
+   */
   function stopCurrentSound() {
     var currentSound = document.getElementById(syncerSounds.currentTime);
 
@@ -69,8 +62,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  // 停止ボタンをクリックした時のイベント
-  document.getElementById(setStopButtonId).onclick = function() {
+  /**
+   * 停止ボタンをクリックした時のイベントを登録する
+   * @returns {boolean}
+   */
+  document.getElementById(stopButtonId).onclick = function () {
     stopCurrentSound();
     return false;
   };
