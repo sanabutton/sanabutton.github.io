23d2e9a0bd58eb456cbe43e40f5e0697bdf6dbc5
ndxbn <sitri.ndxbn@gmail.com>
2018-12-10 07:11:42 +0900

    すべてのボタンを順に再生していくだけのやつを、とりあえず実装してみた。

--- _frontend/main.ts
+++ _frontend/main.ts
@@ -1,1 +1,33 @@
-// nothing
+import SanabuttonClient from "./SanabuttonClient/SanabuttonClient";
+import { HttpClient } from "./SanabuttonClient";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const buttonsRaw = await new SanabuttonClient(HttpClient.create()).buttons();
+  const buttonsFlat: string[] = [];
+  for (const stream of buttonsRaw) {
+    for (const buttonsLine of stream) {
+      for (const button of buttonsLine) {
+        buttonsFlat.push(button["file-name"]);
+      }
+    }
+  }
+
+  await play(buttonsFlat);
+});
+
+async function play(buttons: string[]) {
+  const buttonName = buttons.shift();
+  if(buttonName == null) {
+    return;
+  }
+
+  console.log(buttonName);
+  const audio = new Audio("https://www.natorisana.love/sounds/" + buttonName + ".mp3");
+
+  audio.addEventListener("ended", async () =>{
+    await play(buttons);
+  });
+
+  console.log("start play: " + buttonName);
+  await audio.play();
+}
